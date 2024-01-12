01870aa31a8fd22c7bee380cd9c5359f075c48c1
TraperRoku <119821555+TraperRoku@users.noreply.github.com>
2024-01-12 01:03:20 +0100

    Time: 2 ms (88.08%), Space: 48.1 MB (30.75%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return;
+        }
+        ListNode l1 = head;
+        ListNode prev = null;
+        ListNode fast = head;
+        ListNode slow = head;
+
+
+        while(fast!= null && fast.next!=null){
+           prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+       prev.next = null;
+
+        ListNode l2 = reverse(slow);
+        merge(l1,l2);
+
+    }
+
+    ListNode reverse(ListNode head){
+        ListNode curr = head;
+        ListNode prev = null;
+        while(curr!=null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+
+
+    void merge(ListNode l1, ListNode l2) {
+        while (l1 != null) {
+            ListNode n1 = l1.next;
+            ListNode n2 = l2.next;
+            l1.next = l2;
+
+            if (n1 == null)
+                break;
+
+            l2.next = n1;
+            l1 = n1;
+            l2 = n2;
+        }
+    }
+}
