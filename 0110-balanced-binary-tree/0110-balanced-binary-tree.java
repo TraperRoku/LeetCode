b17949d2560808dae63b0cbc662fee830ffdc7b4
TraperRoku <119821555+TraperRoku@users.noreply.github.com>
2024-01-18 19:07:44 +0100

    Time: 1 ms (42.44%), Space: 44.9 MB (6.61%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -1,19 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isBalanced(TreeNode root) {
     if(root == null) return true;
+
     int left = helper(root.left,0);
     int right = helper(root.right,0);
 
     if(Math.abs(left - right)>1){
         return false;
     }
-    return isBalanced(root.left) && isBalanced(root.right);
+    return isBalanced(root.right) && isBalanced(root.left);
     }
+
 
     private int helper(TreeNode root, int licznik){
         if(root == null) return licznik;
+
         int left = helper(root.left,licznik+1);
         int right = helper(root.right,licznik+1);
-    return Math.max(left,right);
+
+        return Math.max(left,right);
     }
 }
