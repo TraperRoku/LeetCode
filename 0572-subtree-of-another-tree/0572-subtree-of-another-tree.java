49fe4c203bb78f3d548dfa89f9a2f22efae3c7de
TraperRoku <119821555+TraperRoku@users.noreply.github.com>
2024-01-19 03:14:27 +0100

    Time: 2 ms (96.90%), Space: 44.4 MB (20.37%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -15,17 +15,20 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(subRoot == null) return true;
         if(root == null) return false;
-if(finding(root,subRoot)) return true;
-return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
+        if(subRoot == null) return true;
+
+        if(finding(root,subRoot))return true;
+
+        return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
+
     }
 
-    private boolean finding(TreeNode root, TreeNode subRoot){
-        if(root == null && subRoot == null) return true;
+    private boolean finding(TreeNode root , TreeNode subRoot){
+        if(root == null && subRoot == null )return true;
         if(root != null && subRoot != null && root.val == subRoot.val) {
-            return finding(root.left, subRoot.left) && finding(root.right,subRoot.right);
+            return finding(root.left,subRoot.left) && finding(root.right,subRoot.right);
         }
         return false;
-}
+    }
 }
