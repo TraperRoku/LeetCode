e347bfdc6c2bbc1895078fa0af7dfc663c420ae0
TraperRoku <119821555+TraperRoku@users.noreply.github.com>
2024-01-20 00:08:28 +0100

    Time: 1 ms (89.85%), Space: 45.1 MB (17.24%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -17,33 +17,33 @@ import java.util.*;
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-    if(root == null) return new ArrayList<>();
-    List<List<Integer>> list = new ArrayList<>();
 
+        if(root == null) return new ArrayList<>();
+        
         Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> biggerList = new ArrayList<>();
 
         queue.add(root);
 
-        while(!queue.isEmpty()) {
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
 
-            int sizeQueue = queue.size();
 
-            List<Integer> backpack = new ArrayList<>();
 
-            for (int i = 0; i < sizeQueue; i++) {
-
+            for(int i =0; i<size ;i ++){
                 TreeNode temp = queue.poll();
-                backpack.add(temp.val);
 
-                if (temp.left != null) queue.add(temp.left);
-                if (temp.right != null) queue.add(temp.right);
+                if(temp.left!=null) queue.add(temp.left);
+                if(temp.right!=null) queue.add(temp.right);
 
-
+                list.add(temp.val);
             }
-            list.add(backpack);
-        }
-        return list;
-    
-    }
+            biggerList.add(list);
 
+
+        }
+        return biggerList;
+
+    }
 }
