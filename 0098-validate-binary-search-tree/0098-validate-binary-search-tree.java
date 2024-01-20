1926f4cff2023846d77f19dd3564d5aefee8bae2
TraperRoku <119821555+TraperRoku@users.noreply.github.com>
2024-01-20 04:52:41 +0100

    Time: 0 ms (100.00%), Space: 43.2 MB (86.09%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, null, null);
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
-    private boolean isValidBST(TreeNode root, Integer min, Integer max) {
+    private boolean helper(TreeNode root, long left, long right) {
         if (root == null) {
             return true;
         }
 
-        if ((min != null && root.val <= min) || (max != null && root.val >= max)) {
+        if (!(root.val > left && root.val < right)) {
             return false;
         }
 
-        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+        return helper(root.left, left, root.val) && helper(root.right, root.val, right);
     }
 }
