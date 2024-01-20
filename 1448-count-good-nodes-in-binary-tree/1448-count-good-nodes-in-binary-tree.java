43210491ce57065624cb961cb5ed28a1da3b094c
TraperRoku <119821555+TraperRoku@users.noreply.github.com>
2024-01-20 04:27:11 +0100

    Time: 2 ms (74.19%), Space: 51.8 MB (57.81%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -17,15 +17,17 @@ class Solution {
     public int goodNodes(TreeNode root) {
     return helper(root,root.val,0);
     }
-
     private int helper(TreeNode root, int maxVal,int licznik){
         if(root == null) return 0;
-        if(root.val >= maxVal){
+
+        if(root.val>= maxVal){
             licznik++;
         }
-        maxVal = Math.max(root.val,maxVal);
-          licznik += helper(root.left,maxVal,0);
-          licznik+= helper(root.right,maxVal,0);
-            return licznik;
+        maxVal = Math.max(maxVal,root.val);
+
+        licznik+= helper(root.left,maxVal,0);
+        licznik+= helper(root.right,maxVal,0);
+
+        return licznik;
     }
 }
