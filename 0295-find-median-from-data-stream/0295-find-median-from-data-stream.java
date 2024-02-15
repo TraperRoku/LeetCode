9059ea10d9b34973243e5726a5d2bc7add31f764
TraperRoku <119821555+TraperRoku@users.noreply.github.com>
2024-02-15 18:20:07 +0100

    Time: 89 ms (88.94%), Space: 63.7 MB (62.63%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,31 +1,28 @@
 import java.util.PriorityQueue;
 
 class MedianFinder {
-PriorityQueue<Integer> maxHeap;
 PriorityQueue<Integer> minHeap;
-
+PriorityQueue<Integer> maxHeap;
     public MedianFinder() {
-    maxHeap = new PriorityQueue<>();
     minHeap = new PriorityQueue<>((a,b)->b-a);
+    maxHeap = new PriorityQueue<>();
     }
 
     public void addNum(int num) {
-    if(maxHeap.isEmpty() || maxHeap.peek() < num){
+    if(maxHeap.isEmpty() || maxHeap.peek()<num){
         maxHeap.offer(num);
     }else minHeap.offer(num);
 
-    if(maxHeap.size() > minHeap.size() +1){
+    if(maxHeap.size() > minHeap.size()+1){
         minHeap.offer(maxHeap.poll());
-    }else if(maxHeap.size()<minHeap.size()){
+    }else if ( minHeap.size() > maxHeap.size()){
         maxHeap.offer(minHeap.poll());
     }
     }
 
     public double findMedian() {
     if(maxHeap.isEmpty()) return 0;
-    
-    if(maxHeap.size() == minHeap.size()) return (double)(maxHeap.peek()+minHeap.peek())/2;
-    
+    if(maxHeap.size() == minHeap.size()) return (double) (maxHeap.peek() + minHeap.peek())/2;
     return maxHeap.peek();
     }
 }
