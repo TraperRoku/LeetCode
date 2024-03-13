0c5589dd5dfbdf5cac5cd0a224c8a9c1cd79d832
TraperRoku <119821555+TraperRoku@users.noreply.github.com>
2024-03-13 22:58:53 +0100

    Time: 2 ms (99.65%), Space: 43.5 MB (20.59%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,23 +1,31 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-class Solution {
-    List<List<Integer>> result;
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-    result = new ArrayList<>();
-    Arrays.sort(candidates);
-    dfs(candidates,target,new ArrayList<>(),0);
-    return result;
-    }
-    private void dfs(int[] candidates, int target, List<Integer> temp, int start){
-        if(target < 0) return;
-        if(target == 0) result.add(new ArrayList<>(temp));
 
-        for(int i = start; i < candidates.length; i++){
-            if( i > start && candidates[i] == candidates[i-1]) continue;
-            temp.add(candidates[i]);
-            dfs(candidates,target-candidates[i],temp,i+1);
+class Solution {
+    List<List<Integer>> res;
+    public List<List<Integer>> combinationSum2
+            (int[] candidates, int target) {
+        Arrays.sort(candidates);
+        res = new ArrayList<>();
+        dfs(new ArrayList<>(), 0,target , candidates);
+        return res;
+    }
+
+    private void dfs(List<Integer> temp,int index, int target, int[] cand){
+        if(target == 0) res.add(new ArrayList<>(temp));
+        if(target < 0) return;
+
+        int prev = -1;
+        for(int i = index; i < cand.length; i++){
+            if(cand[i] == prev) continue;
+
+            if(cand[i]> target )return;
+            temp.add(cand[i]);
+
+            dfs(temp,i+1,target-cand[i],cand);
             temp.remove(temp.size()-1);
+            prev = cand[i];
         }
     }
 }
