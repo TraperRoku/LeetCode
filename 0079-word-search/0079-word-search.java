45b4c10dccdec2825d141df68125603e2a9dd3bd
TraperRoku <119821555+TraperRoku@users.noreply.github.com>
2024-03-14 22:45:08 +0100

    Time: 134 ms (79.33%), Space: 41.3 MB (76.70%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,29 +1,39 @@
 class Solution {
+    boolean visited[][];
     public boolean exist(char[][] board, String word) {
-        int n = board.length;
-        int m = board[0].length;
-        for(int i = 0; i<n;i++) {
-            for (int j = 0; j < m; j++) {
-                if (dfs(board, i, j, 0,n,m, word)) return true;
+
+         int row = board.length;
+         int col = board[0].length;
+         visited = new boolean[row][col];
+
+         for(int i = 0; i < row; i++){
+             for(int j = 0; j < col; j++){
+            if(dfs(board,i,j,0,word)){
+                return true;
             }
-        }
+             }
+         }
+         return false;
+    }
+    private boolean dfs(char[][] board, int row, int col, int index, String word){
+        if(row == -1 || col == -1 || row == board.length || col == board[0].length){
             return false;
+        }
+        if(visited[row][col] || board[row][col] != word.charAt(index)) return false;
 
-    }
-    private boolean dfs(char[][] board, int i, int j, int count,int n,int m, String word){
-    if(count >= word.length()) return true;
-    if( i < 0 || j<0|| i>= n|| j >= m|| board[i][j] != word.charAt(count)) return false;
-    boolean exist = false;
+        visited[row][col] = true;
 
-    if(board[i][j] == word.charAt(count)){
-        board[i][j] += 100;
-        exist = dfs(board,i+1,j,count+1,n,m,word) ||
-                dfs(board,i,j+1,count+1,n,m,word) ||
-                dfs(board,i-1,j,count+1,n,m,word) ||
-                dfs(board,i,j-1,count+1,n,m,word);
-                board[i][j] -= 100;
-    }
-    return exist;
-    }
+        if(word.length() - 1 == index) return true;
 
+        index++;
+
+        if(dfs(board,row+1,col,index,word) ||
+        dfs(board,row-1,col,index,word) ||
+        dfs(board,row,col -1 ,index, word) ||
+        dfs(board,row, col + 1, index, word)){
+            return true;
+        }
+        visited[row][col] = false;
+        return false;
+    }
 }
